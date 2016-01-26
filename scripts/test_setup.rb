d0e0cb1e732f0117728a57d4d74430ffd9ee9c8c
Jerome Lacoste <jerome.lacoste@gmail.com>
2016-01-26 17:21:51 +0100

    Fix setup check

--- scripts/test_setup.rb
+++ scripts/test_setup.rb
@@ -1,8 +1,12 @@
 require 'dotenv'
 Dotenv.load('fastlane/.env')
+require 'spaceship'
+require 'fastlane_core'
+Helper = FastlaneCore::Helper
 
 ['base', 'tunes'].each do |dir|
   Dir["./scripts/#{dir}/*.rb"].each do |path|
+    puts path
     require path
   end
 end
