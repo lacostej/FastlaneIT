29bef3a4537f56ce95a1eb7c6f09c11bed8ae422
Jerome Lacoste <jerome.lacoste@gmail.com>
2016-02-05 19:26:27 +0100

    Overwrite fixtures at init

--- bin/prepare_fixtures.sh
+++ bin/prepare_fixtures.sh
@@ -2,5 +2,5 @@ if [ ! -f FastlaneIT_fixtures.zip ]; then
   echo "Not in the right directory"
   exit -1
 fi
-unzip FastlaneIT_fixtures.zip
+unzip -o FastlaneIT_fixtures.zip
 cp -a fixtures/android/screenshots_4_5_5/* fixtures/android/metadata_4/
