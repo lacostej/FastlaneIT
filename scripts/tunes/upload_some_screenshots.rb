383de888330c94e0a416cce8e20647c85d4d5612
Jerome Lacoste <jerome.lacoste@gmail.com>
2016-02-05 19:26:55 +0100

    fix path to fixtures

--- scripts/tunes/upload_some_screenshots.rb
+++ scripts/tunes/upload_some_screenshots.rb
@@ -2,7 +2,7 @@ module FastlaneIT
   module Tunes
     class UploadSomeScreenshots < UploadScreenshots
       def perform
-        upload_from("fixtures/screenshots_1_6_5")
+        upload_from("fixtures/ios/screenshots_1_6_5")
       end
     end
   end
