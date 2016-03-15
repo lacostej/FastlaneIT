2a3c4db3a88cf247e26f1fb343a498f870535283
Jerome Lacoste <jerome.lacoste@gmail.com>
2016-03-15 10:03:25 +0100

    Play with multiple set of screenshots to test improved sync

--- bin/prepare_fixtures.sh
+++ bin/prepare_fixtures.sh
@@ -4,3 +4,12 @@ if [ ! -f FastlaneIT_fixtures.zip ]; then
 fi
 unzip -o FastlaneIT_fixtures.zip
 cp -a fixtures/android/screenshots_4_5_5/* fixtures/android/metadata_4/
+
+cp -a find fixtures/ios/screenshots_4_6_5/ find fixtures/ios/screenshots_4_6_1/
+find fixtures/ios/screenshots_4_6_1/ -name "*_[2-5]*.jpg" | xargs rm
+
+cp -a find fixtures/ios/screenshots_4_6_1/ find fixtures/ios/screenshots_4_6_0/
+find fixtures/ios/screenshots_4_6_0/ -name "*.jpg" | xargs rm
+
+cp -a find fixtures/ios/screenshots_4_6_5/ find fixtures/ios/screenshots_4_6_3/
+find fixtures/ios/screenshots_4_6_b/ -name "*_[3-4].jpg" | xargs rm
